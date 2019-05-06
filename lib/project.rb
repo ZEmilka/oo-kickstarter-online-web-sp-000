61f2f68bc1c29f96a3214b3397e4f78659994040
Emilia Ziemichód <emifrompoland@gmail.com>
2019-05-06 20:20:22 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -3,8 +3,8 @@ class Project
   attr_accessor :title, :backers
 
   def initialize(title)
-    @title=title
-    @backers=[]
+    self.title=title#@title=title or 
+    self.backers=[]#@backers=[]
   end
 
   def add_backer(backer)
