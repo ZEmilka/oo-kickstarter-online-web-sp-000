76f93e5409fbdaae9fde9e66edda37a3130cd70d
Emilia Ziemichód <emifrompoland@gmail.com>
2019-05-06 19:50:53 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,15 @@
 class Project
 
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title=title
+    @backers=[]
+  end
+
+  def add_backer(backer)
+    @backer<<backer
+    backer.back_project(self) unless backer.backed_projects.include?(self)
+  end
+
 end
